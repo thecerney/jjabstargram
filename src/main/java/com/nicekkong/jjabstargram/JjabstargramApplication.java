a566ba97479d412e2db4bcdc4b603becc2c556ed
SeoginSon <thecerney@gmail.com>
2020-11-06 14:35:41 +0900

    스프링 부트 생성

---
+++ src/main/java/com/nicekkong/jjabstargram/JjabstargramApplication.java
@@ -0,0 +1,13 @@
+package com.nicekkong.jjabstargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjabstargramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjabstargramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicekkong/jjabstargram/JjabstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicekkong.jjabstargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjabstargramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
